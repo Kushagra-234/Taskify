d3acd1578833deeee3ac805063d6f7fdd1e4ba1a
Kushagra gupta <apple@Apples-MacBook-Pro.local>
2023-07-26 16:41:50 +0530

    fixed breaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,12 @@
-import React, { useState } from "react";
-import "./App.css";
-import Inputfield from "./components/Inputfield";
-import { Todo } from "./model";
-import TodoList from "./components/TodoList";
-// let name: string;
+import React from "react";
 
-// name = "ayush";
-
-// let str: string[];
-
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-
-// let person: Person = {
-//   name: "piyush",
-//   // age: 10,
-// };
-
-// let printName: (name: string) => void;
-
-const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-  console.log(todo);
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([
-        ...todos,
-        {
-          id: Date.now(),
-          todo: todo,
-          isDone: false,
-        },
-      ]);
-      setTodo("");
-    }
+const App = () => {
+  const apiUrl = "https://api.example.com/users";
+  const queryparam = {
+    name: "kush",
+    age: 10,
   };
-
-
-  return (
-    <>
-      <div className="App">
-        <span className="heading">Taskify</span>
-        <Inputfield todo={todo} settodo={setTodo} handleAdd={handleAdd} />
-        <TodoList todos={todos} setTodos={setTodos} />
-      </div>
-    </>
-  );
+  return <div></div>;
 };
 
 export default App;
